503c72677a00ee9b4617d680cbe4c29e3ab6752d
Ibrahima Jephté Traoré <2331689@etu.cegepjonquiere.ca>
2025-11-12 08:19:24 -0500

    page home

---
+++ Web&Python/ProjetWebBd/StationMeteoBlazor/StationMeteoBlazor/Authentification/UserSession.cs
@@ -0,0 +1,12 @@
+﻿namespace StationMeteoBlazor.Authentification
+{
+    public class UserSession
+    {
+        public string UserName { get; set; }
+        public string UserId { get; set; }
+        public string Role { get; set; }
+        public UserSession() {
+
+        }
+    }
+}
